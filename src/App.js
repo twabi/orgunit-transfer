5ad50f3524b7af05a9c3efd0ad1c55c7174cbb47
twabi <itwabi@gmail.com>
2021-10-08 19:33:17 +0200

    posting stratums

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ function App(props) {
   const [statusText, setStatusText] = useState("normal");
   const [messageText, setMessageText] = useState("Checking stratum...");
   const [selectedUnit, setSelectedUnit] = useState(null);
-  const [textValue, setTextValue] = useState(null);
   const [auth, setAuth] = useState(props.auth);
   const [treeMarkets, setTreeMarkets] = useState(props.orgUnits);
   const [treeValue, setTreeValue] = useState();
   const [flattenedUnits, setFlattenedUnits] = useState([]);
   const [unitGroups, setUnitGroups] = useState(props.unitGroups);
-  const [selectedInstance, setSelectedInstance] = useState("NamisDemo")
+  const [selectedInstance, setSelectedInstance] = useState("NamisDemo");
+  const [loading, setLoading] = useState(false);
 
   let extractChildren = x => x.children;
   let flatten = (children, getChildren, level, parent) => Array.prototype.concat.apply(
@@ -61,128 +61,175 @@ function App(props) {
   },[props]);
 
   const postingUnit = (unit) => {
-    var schemaLoad = {
-      attributeValues: unit.attributeValues,
-      lastUpdated: unit.lastUpdated,
-      name: unit.name,
-      openingDate: unit.openingDate,
-      parent: {id: unit.parent},
-      shortName: unit.shortName,
-      path: unit.path,
-      created: unit.created,
-      lastUpdatedBy:{
-        id: unit.lastUpdatedBy.id
-      },
-      createdBy:{
-        id: unit.createdBy.id
-      },
-      translations: unit.translations
-    }
+    console.log(unit);
+    if(unit.id == undefined || unit.id == null){
 
-    var payload = {
-      ancestors: unit.ancestors,
-      attributeValues: unit.attributeValues,
-      created: unit.created,
-      dataSets: unit.dataSets,
-      dimensionItem: unit.dimensionItem,
-      dimensionItemType: unit.dimensionItemType,
-      displayFormName: unit.displayFormName,
-      displayName: unit.displayName,
-      displayShortName: unit.displayShortName,
-      id: unit.id,
-      lastUpdated: unit.lastUpdated,
-      level: unit.level,
-      name: unit.name,
-      openingDate: unit.openingDate,
-      organisationUnitGroups: unit.organisationUnitGroups,
-      parent: unit.parent,
-      path: unit.path,
-      periodOffset: unit.periodOffset,
-      programs: unit.programs,
-      shortName: unit.shortName,
-      translations: unit.translations,
-      lastUpdatedBy:{
-        id: unit.lastUpdatedBy.id
-      },
-      createdBy:{
-        id: unit.createdBy.id
-      },
-    }
+    } else {
+      var schemaLoad = {
+        "path": unit.path,
+        "lastUpdated": unit.lastUpdated,
+        "id": unit.id,
+        "level": unit.level,
+        "created": unit.created,
+        "attributeValues": unit.attributeValues,
+        "name": unit.name,
+        "shortName": unit.shortName,
+        "openingDate": unit.openingDate,
+        "parent":{
+          "id": unit.parent.id
+        },
+        "lastUpdatedBy":{
+          "id": unit.lastUpdatedBy.id
+        },
+        "createdBy":{
+          "id": unit.createdBy.id
+        },
+        "translations": unit.translations
+      }
 
-    setStatus(10);
-    setMessageText("Posting children of " + unit.displayName);
-    setStatusText("normal");
+      /*
+      var payload = {
+        "ancestors": unit.ancestors,
+        "attributeValues": unit.attributeValues,
+        "created": String(unit.created),
+        "dataSets": unit.dataSets,
+        "dimensionItem": unit.dimensionItem,
+        "dimensionItemType": unit.dimensionItemType,
+        "displayFormName": unit.displayFormName,
+        "displayName": unit.displayName,
+        "displayShortName": unit.displayShortName,
+        "id": unit.id,
+        "lastUpdated": String(unit.lastUpdated),
+        "level": unit.level,
+        "name": unit.name,
+        "openingDate": String(unit.openingDate),
+        "organisationUnitGroups": unit.organisationUnitGroups,
+        "parent": unit.parent,
+        "path": unit.path,
+        "periodOffset": unit.periodOffset,
+        "programs": unit.programs,
+        "shortName": unit.shortName,
+        "translations": unit.translations,
+        "lastUpdatedBy":{
+          "id": unit.lastUpdatedBy.id
+        },
+        "createdBy":{
+          "id": unit.createdBy.id
+        },
+      }*/
 
 
-    console.log(schemaLoad);
-    console.log(payload);
+      //console.log(schemaLoad);
+      //console.log(payload);
 
-    fetch(schemaPoint, {
-      method: 'POST',
-      body: JSON.stringify(schemaLoad),
-      headers: {
-        'Authorization' : auth,
-        'Content-type': 'application/json',
-      },
-      credentials: "include"
 
-    }).then((response) => {
-      if(response.status === 200 || response.status === 201){
-        setStatus(65);
+      fetch(schemaPoint, {
+        method: 'POST',
+        body: JSON.stringify(schemaLoad),
+        headers: {
+          'Authorization' : auth,
+          'Content-type': 'application/json',
+        },
+        credentials: "include"
 
-        fetch(unitPoint, {
-          method: 'POST',
-          body: JSON.stringify(payload),
-          headers: {
-            'Authorization' : auth,
-            'Content-type': 'application/json',
-          },
-          credentials: "include"
+      }).then((response) => {
+        console.log(response);
+        if(response.status === 200 || response.status === 201){
+          setStatus(65);
 
-        }).then((response) => {
+          fetch(unitPoint, {
+            method: 'POST',
+            body: JSON.stringify(schemaLoad),
+            headers: {
+              'Authorization' : auth,
+              'Content-type': 'application/json',
+            },
+            credentials: "include"
 
-          console.log(response);
-          if(response.status === 200 || response.status === 201){
-            setTimeout(() => {
-              setMessageText("Org Unit posted");
-              setStatusText("success");
+          }).then((response) => {
+
+            console.log(response);
+            if(response.status === 200 || response.status === 201){
+              setTimeout(() => {
+                setMessageText("Org Unit posted");
+                setStatusText("success");
+                setStatus(100);
+              }, 2000);
+
+            } else {
+              setMessageText("Unable to post org units due to an error");
+              setStatusText("exception");
               setStatus(100);
-            }, 2000);
-
-          }else if(response.status === 409 ){
-            setMessageText("Org Unit already exists on the server");
+            }
+          }).catch((error) => {
+            setMessageText("Unable to update org units due to an error : " + error.message);
             setStatusText("exception");
             setStatus(100);
+          });
+        }else {
+          setMessageText("Unable to post org units due to an error");
+          setStatusText("exception");
+          setStatus(100);
+        } //
+      });
+    }
 
-          } else {
-            setMessageText("Unable to post org units due to an error");
-            setStatusText("exception");
-            setStatus(100);
-          }
-        })
-      } //
-    });
+
+
   }
 
   const handleCorrective =()=>{
     console.log(flattenedUnits);
 
+    setLoading(false);
     flattenedUnits.map((unit) => {
+      setStatus(10);
+      setMessageText("Posting children of " + flattenedUnits[0].displayName);
+      setStatusText("normal");
       postingUnit(unit);
     });
   }
 
   const handleDemo = () => {
     console.log(selectedUnit);
-    if(selectedUnit !== null){
+    if(selectedUnit == null){
+      alert("no stratum has been selected. Choose a stratum by clicking your choice in the dropdown");
+      setLoading(false);
+      setAlertModal(false);
+    }else{
+
+      setLoading(false);
 
       var group = unitGroups[unitGroups.findIndex(x => x.id === selectedUnit)]
       console.log(group);
-      group.children.map((child) => {
-        setStatus(30);
-        const endpoint = `organisationUnits/${child.id}.json?fields=*`;
 
-        fetch(`https://covmw.com/namisdemo/api/${endpoint}`, {
+      setStatus(10);
+      setMessageText("Posting children of " + group.displayName);
+      setStatusText("normal");
+
+      const unitPoint = "organisationUnits.json?paging=false&fields=id,displayName";
+      fetch(`https://covmw.com/namisdemo/api/${unitPoint}`, {
+        method: 'GET',
+        headers: {
+          'Authorization' : auth,
+          'Content-type': 'application/json',
+        },
+        credentials: "include"
+
+      }).then(result => result.json()).then((response) =>{
+
+        setStatus(30);
+        console.log(response);
+
+        var unitArray = response.organisationUnits;
+        console.log(group.displayName, "Dinji stratum");
+        var object = unitArray[unitArray.findIndex(x => x.displayName.toLowerCase() === group.displayName.toLowerCase())]
+        console.log(object);
+
+        var stratumArray = group.organisationUnits;
+
+        console.log("just before");
+        fetch(`https://covmw.com/namisdemo/api/organisationUnits/${object.id}.json?fields=*`, {
           method: 'GET',
           headers: {
             'Authorization' : auth,
@@ -191,14 +238,49 @@ function App(props) {
           credentials: "include"
 
         }).then(result => result.json()).then((response) => {
-          setStatus(50);
-              postingUnit(response);
+          console.log(response);
+          postingUnit(response);
+          setStatus(32);
+
+        }).then(() => {
+
+          stratumArray.map((child) => {
+
+            setStatus(35);
+            if(child === undefined){
+            } else {
+
+              const endpoint = `organisationUnits/${child.id}.json?fields=*`;
+
+              fetch(`https://covmw.com/namisdemo/api/${endpoint}`, {
+                method: 'GET',
+                headers: {
+                  'Authorization' : auth,
+                  'Content-type': 'application/json',
+                },
+                credentials: "include"
+
+              }).then(result => result.json()).then((response) => {
+
+                setStatus(50);
+                postingUnit(response);
+
+              });
+            }
+
+          });
         });
+
+
       });
+
+
     }
   }
 
   const handlePost = () => {
+    setLoading(true);
+    setAlertModal(true);
     if(selectedInstance === "NamisDemo"){
       handleDemo()
     } else {
@@ -247,8 +329,8 @@ function App(props) {
               <Row className="w-75 mt-3">
                 <Col span={12} className="p-3 text-left">
                   <SelectField width="100%"
-                               label="Select organization Unit Group"
-                               description="Select the stratum whose children you wish to change"
+                               label="Select DHIS2 Instance"
+                               description="Select the instance to transfer orgUnits from"
                                value={selectedInstance}
                                onChange={e => setSelectedInstance(e.target.value)}>
                     {['NamisDemo', 'Correctiveinstance'].map((item) => (
@@ -263,8 +345,8 @@ function App(props) {
                 <Col span={12} className="p-3 text-left">
                   {selectedInstance === "NamisDemo" ?
                       <SelectField width="100%"
-                                   label="Select DHIS2 Instance"
-                                   description="Select the instance to transfer orgUnits from"
+                                   label="Select organization Unit Group"
+                                   description="Select the stratum whose children you wish to post"
                                    value={selectedUnit&&selectedUnit.displayName}
                                    onChange={e => setSelectedUnit(e.target.value)}>
                         {unitGroups&&unitGroups.map((unit) => (
@@ -304,7 +386,9 @@ function App(props) {
               <Row className="w-25 mt-4">
                 <Col span={24}>
                   <Button appearance="primary" onClick={handlePost}>
-                    POST
+                    POST {loading ? <div className="spinner-border mx-2 text-white spinner-border-sm" role="status">
+                    <span className="sr-only">Loading...</span>
+                  </div> : null}
                   </Button>
                 </Col>
               </Row>
